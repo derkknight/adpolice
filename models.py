5655cc809a7a36813fa8ad59f85a79e84fe7bff9
Jason Pruski <jprus@localhost.localdomain>
2018-06-19 12:42:45 -0400

    scaffold

---
+++ models.py
@@ -0,0 +1,33 @@
+import psycopg2
+import secret
+
+
+try:
+	conn = psycopg2.connect(secret.connect_str)
+	cur = conn.cursor()
+except Exception as e:
+	print("Uh oh, can't connect. Invalid dbname, user or password?")
+	print(e)
+	
+def get_submissions(loginfo):
+	sql_statement = "SELECT * FROM submissions;"
+	cur.execute(sql_statement, loginfo)
+	return cur.fetchone()
+
+def create_submission(url, score):
+	cur.execute("SELECT * FROM submissions WHERE url=%s;", (url,))
+	if cur.fetchone():
+		return False
+	else:
+		sql_statement = """
+		INSERT INTO submissions
+		(url, score)
+		VALUES
+		(%s,%s);
+		"""
+		cur.execute(sql_statement, (url, score))
+		_id = cur.fetchone()[0]
+		conn.commit()
+		if _id:
+			return 1
+		return False
