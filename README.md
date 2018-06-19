26141b2b2dec90c721f86a87a83a6d84cac35e78
Jason Pruski <jpru5@yandex.com>
2018-06-19 14:49:12 -0400

    add file upload

--- README.md
+++ README.md
@@ -2,10 +2,20 @@
 
 create a postgres database called adpolice
 
-change password in secret.py to match your postgres password
+in the command line run the following shell commands:
 
-in the command line run:
+1) git clone https://github.com/derkknight/adpolice.git
 
-python schema.py
+2) cd to the directory adpolice/
 
-python controller.py
+3) python3 -m venv env
+
+4) source env/bin/activate
+
+5) pip install -r requirements.txt 
+
+open secret.py in your editor and then change "password='nosagedbhaslo'" to match your postgres password
+
+6) python schema.py
+
+7) python controller.py
--- controller.py
+++ controller.py
@@ -1,18 +1,28 @@
-
 import os
 from flask import Flask, request, redirect, render_template
+from flask_uploads import UploadSet, configure_uploads, IMAGES
 import models
 
 app = Flask(__name__) # create the application instance :)
 app.secret_key = os.urandom(24)
+
+photos = UploadSet('photos', IMAGES)
+
+app.config['UPLOADED_PHOTOS_DEST'] = 'static/img'
+configure_uploads(app, photos)
 
 @app.route('/', methods=["GET"])
 def show_home():
 	return render_template("index.html", error="")
 
 @app.route('/results', methods=["POST"])
-def edit(post_id=None):
-	url = request.form.get('url')
+def review(post_id=None):
+	url = request.form.get('website_url')
+	if 'photo' in request.files:
+		# file name for photo
+		filename = photos.save(request.files['photo'])
+	else:
+		return render_template("index.html", error="Invalid Submission")
 	if models.create_submission(url, 5):
 		page_data = {'url':url, 'score':5}
 		return render_template("results.html", page_data=page_data)
