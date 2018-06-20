c2b96b068037a0a17b4d176b945662f2a62a0078
Jason Pruski <jpru5@yandex.com>
2018-06-19 22:46:09 -0400

    list recordss of submissions

--- controller.py
+++ controller.py
@@ -24,20 +24,46 @@ def review(post_id=None):
 	print(request.files)
 	file_name = ""
 	if 'photo' in request.files:
-		# file name for photo
 		file_name = photos.save(request.files['photo'])
 		print('file name', file_name)
 	else:
 		return render_template("index.html", error="File wasn't uploaded.")
 	if url:
-		#page_data = {'url':url, 'score':5}
 		page_data = police_ads.police_ad('static/img/' + file_name, url)
-		#models.create_submission(url, 5)
 		print(page_data)
 		return render_template("results.html", page_data=page_data)
 	else:
 		print("no URL", url)
 		return render_template("index.html", error="No URL to the company website.")
+
+@app.route('/ads', methods=["GET"])
+def show_ads(item_id=None, action=None):
+	page_data = {}
+	if item_id and action:
+		page_data['context'] = models.get_items(item_id, action)
+		return page_data
+	else:
+		page_data['page_num'] = 0
+		page_data['need_previous'] = False
+		page_data['context'] = models.get_items()
+		return render_template("ads.html", page_data=page_data)
+
+@app.route('/next/<item_id>/<page_num>', methods=["GET"])
+def next_5(item_id=None, page_num=None):
+	page_data = show_home(item_id, 'next')
+	page_data['page_num'] = int(page_num) + 1
+	page_data['need_previous'] = True
+	return render_template("index.html", page_data=page_data)
+
+@app.route('/last/<item_id>/<page_num>', methods=["GET"])
+def last_5(item_id=None, page_num=None):
+	page_data = show_home(item_id, 'last')
+	page_data['page_num'] = int(page_num) - 1
+	if page_data['page_num'] == 0:
+		page_data['need_previous'] = False
+	else:
+		page_data['need_previous'] = True
+	return render_template("index.html", page_data=page_data)
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- models.py
+++ models.py
@@ -8,11 +8,6 @@ try:
 except Exception as e:
 	print("Uh oh, can't connect. Invalid dbname, user or password?")
 	print(e)
-	
-def get_submissions(loginfo):
-	sql_statement = "SELECT * FROM submissions;"
-	cur.execute(sql_statement, loginfo)
-	return cur.fetchone()
 
 def create_submission(url, score):
 	sql_statement = """
@@ -28,3 +23,41 @@ def create_submission(url, score):
 	if _id:
 		return 1
 	return False
+
+def clean_data(all_submissions, reverse):
+	context = {
+		'items':[]
+		}
+	for i, this_item in enumerate(all_submissions):
+		if i==0:
+			if reverse:
+				context['last'] = this_item[0]
+			else:
+				context['first'] = this_item[0]
+		if i==4:
+			if reverse:
+				context['first'] = this_item[0]
+			else:
+				context['last'] = this_item[0]
+		context['items'].append({
+			'id': this_item[0],
+			'url': this_item[1],
+			'image_url': this_item[2],
+			'score': this_item[3]
+		})
+	return context
+	
+def get_items(item_id=None, action=None):
+	reverse = False
+	if item_id:
+		if action == 'next':
+			q_string = "SELECT * FROM submissions WHERE pk<%s ORDER BY pk DESC LIMIT 5;"
+		else:
+			reverse = True
+			q_string = "SELECT * FROM submissions WHERE pk>%s ORDER BY pk ASC LIMIT 5;"
+		cur.execute(q_string, (item_id,))
+	else:
+		q_string = "SELECT * FROM submissions ORDER BY pk DESC LIMIT 5;"
+		cur.execute(q_string)
+	all_submissions = cur.fetchall()
+	return clean_data(all_submissions, reverse)
